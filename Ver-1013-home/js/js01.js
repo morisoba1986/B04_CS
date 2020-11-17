4e57c9aecd83a380a912b0c6a7800d24881ab474
morisoba1986 <morisoba19860802@gmail.com>
2020-11-17 14:11:02 +0900

    Add files via upload

--- Ver-1013-home/js/js01.js
+++ Ver-1013-home/js/js01.js
@@ -1,29 +1,33 @@
-function downloadFile(url,filename){
-    "use strict";
-    console.log("called");
-    let xhr=new XMLHttpRequest();
-    xhr.open("GET",url,true);
-    xhr.responseType="blob";
-    xhr.onload=function(oEvent){
-        let blob=xhr.response;
-        if(window.navigator.msSaveBlob){
-            window.navigator.msSaveBlob(blob,filename);
-        }
-        else{
-            let objectURL=window.URL.createObjectURL(blob);
-            let link=document.createElement("a");
-            document.body.appendChild(link);
-            link.href=objectURL;
-            link.download=filename;
-            link.click();
-            document.body.removeChild(link);
-        }
-    };
-    xhr.send();
-    let top=window.innerHeight/2;
-    let left=window.innerWidth/2;
-    window.open("downloader.html",null,'top=,left=left,width=left,height=top');
-    setTimeout(() => {
-        window.close();
-    }, 500);
+// function downloadFile(url,filename){
+//     "use strict";
+//     console.log("called");
+//     let xhr=new XMLHttpRequest();
+//     xhr.open("GET",url,true);
+//     xhr.responseType="blob";
+//     xhr.onload=function(oEvent){
+//         let blob=xhr.response;
+//         if(window.navigator.msSaveBlob){
+//             window.navigator.msSaveBlob(blob,filename);
+//         }
+//         else{
+//             let objectURL=window.URL.createObjectURL(blob);
+//             let dlImage=document.createElement("a");
+//             document.body.appendChild(dlImage);
+//             dlImage.href=objectURL;
+//             dlImage.download=filename;
+//             dlImage.click();
+//             document.body.removeChild(dlImage);
+//         }
+//     };
+//     xhr.send();
+window.onload = function () {
+    let buttun = document.getElementById('downloader');
+    buttun.onclick = pushedButton;
+    function pushedButton() {
+        window.open("downloader.html", null, 'top=1,left=1,width=1,height=1');
+    }
 }
+    // setTimeout(() => {
+    //     window.close();
+    // }, 500);
+// }
---
+++ Ver-1013-home/js/js02.js
@@ -0,0 +1,32 @@
+window.onload = downloadFile;
+
+
+function downloadFile(url, filename) {
+    let link = "test.zip";
+    window.open(link, '_self');
+    // "use strict";
+    // console.log("called");
+    // let xhr = new XMLHttpRequest();
+    // xhr.open("GET", url, true);
+    // xhr.responseType = "blob";
+    // xhr.onload = function (oEvent) {
+    //     let blob = xhr.response;
+    //     if (window.navigator.msSaveBlob) {
+    //         window.navigator.msSaveBlob(blob, filename);
+    //     }
+    //     else {
+    //         let objectURL = window.URL.createObjectURL(blob);
+    //         let dlImage = document.createElement("a");
+    //         document.body.appendChild(dlImage);
+    //         dlImage.href = objectURL;
+    //         dlImage.download = filename;
+    //         dlImage.click();
+    //         document.body.removeChild(dlImage);
+    //     }
+    // };
+    // xhr.send();
+    // window.open("downloader.html",null,'top=,left=left,width=left,height=top');
+    setTimeout(() => {
+        window.close();
+    }, 250);
+}
