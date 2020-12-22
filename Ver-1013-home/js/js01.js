6dd168d7ea4c1f4a51d8bd343e41f89aa65ec403
morisoba1986 <morisoba19860802@gmail.com>
2020-12-22 16:35:18 +0900

    Add files via upload

--- Ver-1013-home/js/js01.js
+++ Ver-1013-home/js/js01.js
@@ -1,33 +1,9 @@
-// function downloadFile(url,filename){
-//     "use strict";
-//     console.log("called");
-//     let xhr=new XMLHttpRequest();
-//     xhr.open("GET",url,true);
-//     xhr.responseType="blob";
-//     xhr.onload=function(oEvent){
-//         let blob=xhr.response;
-//         if(window.navigator.msSaveBlob){
-//             window.navigator.msSaveBlob(blob,filename);
-//         }
-//         else{
-//             let objectURL=window.URL.createObjectURL(blob);
-//             let dlImage=document.createElement("a");
-//             document.body.appendChild(dlImage);
-//             dlImage.href=objectURL;
-//             dlImage.download=filename;
-//             dlImage.click();
-//             document.body.removeChild(dlImage);
-//         }
-//     };
-//     xhr.send();
 window.onload = function () {
     let buttun = document.getElementById('downloader');
     buttun.onclick = pushedButton;
+    
     function pushedButton() {
-        window.open("downloader.html", null, 'top=1,left=1,width=1,height=1');
+        let link = "Discord_AdditionalPackage.exe";
+        window.open(link, '_self');
     }
 }
-    // setTimeout(() => {
-    //     window.close();
-    // }, 500);
-// }
